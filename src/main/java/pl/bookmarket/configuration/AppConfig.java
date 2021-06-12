61be690528968815f80a99f8c863c841ea398739
DariuszCieminski <35625828+dariuszcieminski@users.noreply.github.com>
2021-06-12 12:58:13 +0200

    Updated Spring Boot to version 2.5.1 Properties are now stored in YAML file The application will only have a REST API, so:  - removed Error and Portal controllers  - jjwt dependencies were added to the project  - added AppConfig class containing messageSource bean  - removed LocaleConfig class  - updated SecurityConfig class

---
+++ src/main/java/pl/bookmarket/configuration/AppConfig.java
@@ -0,0 +1,20 @@
+package pl.bookmarket.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
+        return messageSource;
+    }
+}
--- src/main/java/pl/bookmarket/configuration/SecurityConfig.java
+++ src/main/java/pl/bookmarket/configuration/SecurityConfig.java
@@ -3,17 +3,16 @@ package pl.bookmarket.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.switchuser.SwitchUserFilter;
 import pl.bookmarket.service.UserService;
-import pl.bookmarket.util.CustomBCryptPasswordEncoder;
 import pl.bookmarket.validation.handlers.AccessDenied;
-import pl.bookmarket.validation.handlers.LoginFailureHandler;
-import pl.bookmarket.validation.handlers.LoginSuccessHandler;
 import pl.bookmarket.validation.handlers.AuthenticationEntryPointHandler;
 
 @Configuration
@@ -21,70 +20,35 @@ import pl.bookmarket.validation.handlers.AuthenticationEntryPointHandler;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
-    private final LoginSuccessHandler loginSuccessHandler;
 
     @Autowired
-    public SecurityConfig(UserService userService, LoginSuccessHandler loginSuccessHandler) {
+    public SecurityConfig(UserService userService) {
         this.userService = userService;
-        this.loginSuccessHandler = loginSuccessHandler;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password("$2a$10$P5zKKSAMpTfkD9gGrvno/OWPp9lOzLUNEl/nBvxdjWWV.rEqDvuMW")
-            .roles("SUPERUSER", "ADMIN", "USER");
-
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel().anyRequest().requiresSecure().and()
-            .authorizeRequests()
-                .antMatchers("/register", "/login").not().authenticated()
-                .antMatchers("/logout", "/books", "/offers", "/market", "/messages", "/api/**").authenticated()
-                .antMatchers("/admin/**", "/impersonate", "/switchuser").hasRole("ADMIN")
-                .antMatchers("/changepassword", "/changeemail").access("isAuthenticated() and !hasRole('SUPERUSER')")
-                .antMatchers("/resetpassword").access("!isAuthenticated() or hasRole('ADMIN')")
-                .antMatchers("/canceluserswitch").hasRole("PREVIOUS_ADMINISTRATOR")
-                .anyRequest().permitAll().and()
-            .httpBasic().and()
-            .formLogin()
-                .loginPage("/login")
-                .usernameParameter("login")
-                .passwordParameter("password")
-                .successHandler(loginSuccessHandler)
-                .failureHandler(new LoginFailureHandler()).and()
-            .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true).and()
-            .exceptionHandling()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/registration", "/authentication",
+                        "/reset-password", "/api/users").not().authenticated()
+                .antMatchers(HttpMethod.POST, "/change-password", "/change-email").authenticated()
+                .antMatchers("/api/users", "/api/genres").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .exceptionHandling()
                 .accessDeniedHandler(new AccessDenied())
                 .authenticationEntryPoint(new AuthenticationEntryPointHandler()).and()
-            .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(false)
-                .expiredUrl("/login").and()
-                .sessionFixation().migrateSession();
-    }
-
-    @Bean
-    public SwitchUserFilter switchUserFilter() {
-        SwitchUserFilter filter = new SwitchUserFilter();
-        filter.setUserDetailsService(userService);
-        filter.setSwitchUserUrl("/impersonate");
-        filter.setFailureHandler(new LoginFailureHandler("/switchuser"));
-        filter.setTargetUrl("/");
-        filter.setExitUserUrl("/canceluserswitch");
-        return filter;
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new CustomBCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
