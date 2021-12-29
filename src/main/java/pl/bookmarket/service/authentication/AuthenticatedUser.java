75b3ee56b973caeb066cb790f563c07979c20f68
Darek <d.cieminski@gmail.com>
2021-12-29 23:43:48 +0100

    Created AuthenticatedUser class, custom implementation of UserDetails interface. Refactored DatabaseUserDetailsService class to use the new implementation. Changed the way user is loaded: by email address instead of login.

---
+++ src/main/java/pl/bookmarket/service/authentication/AuthenticatedUser.java
@@ -0,0 +1,179 @@
+package pl.bookmarket.service.authentication;
+
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class AuthenticatedUser implements UserDetails, CredentialsContainer {
+
+    private String password;
+
+    private final String username;
+
+    private final Long id;
+
+    private final Set<GrantedAuthority> authorities;
+
+    private final boolean accountNonExpired;
+
+    private final boolean accountNonLocked;
+
+    private final boolean credentialsNonExpired;
+
+    private final boolean enabled;
+
+    public AuthenticatedUser(String username, String password, Long id, Set<GrantedAuthority> authorities) {
+        this(username, password, id, authorities, true, true, true, true);
+    }
+
+    public AuthenticatedUser(String username, String password, Long id, Set<GrantedAuthority> authorities, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled) {
+        Assert.hasLength(username, "Username must not be empty!");
+        Assert.hasLength(password, "Password must not be empty!");
+        Assert.notNull(id, "User ID must not be null!");
+        Assert.notEmpty(authorities, "Must contain at least 1 authority!");
+
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.authorities = Collections.unmodifiableSortedSet(prepareAuthorities(authorities));
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private SortedSet<GrantedAuthority> prepareAuthorities(Set<GrantedAuthority> authorities) {
+        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(Comparator.comparing(GrantedAuthority::getAuthority));
+
+        authorities.forEach(grantedAuthority -> {
+            Assert.isTrue(!grantedAuthority.getAuthority()
+                                           .startsWith("ROLE_"), "Authority must not start with ROLE_ prefix!");
+            sortedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + grantedAuthority.getAuthority()));
+        });
+
+        return sortedAuthorities;
+    }
+
+    public static AuthenticatedUserBuilder builder() {
+        return new AuthenticatedUserBuilder();
+    }
+
+    public static class AuthenticatedUserBuilder {
+        private String username;
+
+        private String password;
+
+        private Long id;
+
+        private List<GrantedAuthority> authorities;
+
+        private boolean accountExpired;
+
+        private boolean accountLocked;
+
+        private boolean credentialsExpired;
+
+        private boolean disabled;
+
+        public AuthenticatedUserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder authorities(String... authorities) {
+            this.authorities = AuthorityUtils.createAuthorityList(authorities);
+            return this;
+        }
+
+        public AuthenticatedUserBuilder accountExpired(boolean accountExpired) {
+            this.accountExpired = accountExpired;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder accountLocked(boolean accountLocked) {
+            this.accountLocked = accountLocked;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder credentialsExpired(boolean credentialsExpired) {
+            this.credentialsExpired = credentialsExpired;
+            return this;
+        }
+
+        public AuthenticatedUserBuilder disabled(boolean disabled) {
+            this.disabled = disabled;
+            return this;
+        }
+
+        public AuthenticatedUser build() {
+            return new AuthenticatedUser(this.username, this.password, this.id, new HashSet<>(this.authorities),
+                    !this.accountExpired, !this.accountLocked, !this.credentialsExpired, !this.disabled);
+        }
+    }
+}
--- src/main/java/pl/bookmarket/service/authentication/DatabaseUserDetailsService.java
+++ src/main/java/pl/bookmarket/service/authentication/DatabaseUserDetailsService.java
@@ -1,43 +1,35 @@
 package pl.bookmarket.service.authentication;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import pl.bookmarket.dao.UserDao;
+import pl.bookmarket.model.Role;
 import pl.bookmarket.model.User;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.springframework.security.core.userdetails.User.builder;
+import pl.bookmarket.service.crud.UserService;
 
 @Service
 public class DatabaseUserDetailsService implements UserDetailsService {
 
-    private final UserDao dao;
+    private final UserService userService;
 
-    @Autowired
-    public DatabaseUserDetailsService(UserDao dao) {
-        this.dao = dao;
+    public DatabaseUserDetailsService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = dao.findUserByLogin(username)
-                       .orElseThrow(() -> new UsernameNotFoundException("There is no user with login: " + username));
+        User user = userService.getUserByEmail(username)
+                               .orElseThrow(() -> new UsernameNotFoundException("The user with following email address does not exist: " + username));
 
-        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName())));
+        String[] roles = user.getRoles().stream().map(Role::getName).toArray(String[]::new);
 
-        UserBuilder userBuilder = builder();
-        return userBuilder.username(username)
-                          .password(user.getPassword())
-                          .disabled(user.isBlocked())
-                          .authorities(authorities)
-                          .build();
+        return AuthenticatedUser.builder()
+                                .username(user.getLogin())
+                                .password(user.getPassword())
+                                .id(user.getId())
+                                .authorities(roles)
+                                .accountLocked(user.isBlocked())
+                                .build();
     }
 }
